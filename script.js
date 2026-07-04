cdcf2390f067d3d4de681d55e386fc9c3e20a201
aidengeorge920-commits <aiden.george920@gmail.com>
2026-07-04 17:31:21 +1000

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@
 
 const GALLERY = [
   { src: "images/exterior1.jpg", caption: "New build — exterior" },
-  { src: "images/exterior2.jpg", caption: "New build — exterior" },
   { src: "images/kitchen1.jpg", caption: "New build — kitchen" },
   { src: "images/bathroom1.jpg", caption: "New build — bathroom" },
   { src: "images/lounge1.jpg", caption: "New build — living area" },
